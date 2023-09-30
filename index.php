a77baa8338802246a667b7072889ad792a67d923
kunalc881 <122966979+kunalc881@users.noreply.github.com>
2023-09-30 18:15:02 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My PHP page using openshift S2I </h1>
+   <h2> welcome to updated version thank you </h2>
+<?php
+echo "Hello fom kunal welcome to S2I Webhooks..... !";
+?>
+
+</body>
+</html>
