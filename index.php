924927df9909b1f1edaa8c56d6fc1818681f8e6e
kunalc881 <122966979+kunalc881@users.noreply.github.com>
2023-10-02 17:12:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <h1>My PHP page using openshift S2I </h1>
    <h2> welcome to updated version thank you </h2>
 <?php
-echo "Hello fom kunal welcome to S2I Webhooks..... !";
+echo "Hello fom kunal chavan welcome to S2I Webhooks..... !";
 ?>
 
 </body>
